5393aaca2c1efccaa22deea32f03a54a85406164
bqm3 <bqminh30@gmail.com>
2024-05-07 16:19:38 +0700

    update

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const app = express();
 
 
 var corsOptions = {
-  origin: ["*", "localhost:3000"],
+  origin: ["*", "http://localhost:3000","localhost:3000"],
 
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
