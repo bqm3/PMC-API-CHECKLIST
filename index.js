e8a2ddb449be97919d1cd74a869698e30cae604a
bqm3 <bqminh30@gmail.com>
2024-05-07 16:22:10 +0700

    update cors

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-
 var corsOptions = {
-  origin: ["*", "http://localhost:3000","localhost:3000"],
+  origin: [
+    "*",
+    "http://localhost:3000",
+    "localhost:3000",
+    "https://dawndev.io.vn",
+    "https://master-nu-pied.vercel.app/",
+    "checklist.pmcweb.vn",
+  ],
 
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-app.use(cors())
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json({ limit: "500mb" }));
