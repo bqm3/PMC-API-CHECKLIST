ce17b952b624387d49a2dfde7e0903f862d88f61
bqm3 <bqminh30@gmail.com>
2024-07-05 07:59:34 +0700

    update api create

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -7,8 +7,15 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.DB_PORT
-  }
+    port: process.env.DB_PORT,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+  
 );
 
 sequelize
