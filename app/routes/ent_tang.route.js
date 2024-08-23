5e5bc83c3c4ebada4f31d81cc577b8d8bea7fae9
bqm3 <bqminh30@gmail.com>
2024-08-23 09:57:18 +0700

    update file tieuchuan, delete mul tang

--- app/routes/ent_tang.route.js
+++ app/routes/ent_tang.route.js
@@ -9,6 +9,7 @@ module.exports = (app) => {
 
   router.post("/create", [isAuthenticated, isAdmin], ent_tang.create);
   router.get("/", [isAuthenticated], ent_tang.get);
+  router.put("/delete-mul", [isAuthenticated], ent_tang.deleteMul)
 
   app.use("/api/ent_tang", router);
 };
