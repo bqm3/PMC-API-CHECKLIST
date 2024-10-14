d97caeaffdd4ac52e7425088c550224e3271b054
bqm3 <bqminh30@gmail.com>
2024-10-14 08:11:42 +0700

    add backup csdl

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 require("dotenv").config();
+const cron = require("node-cron");
 const express = require("express");
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
+const PDFDocument = require("pdfkit");
 const cookieParser = require("cookie-parser");
+const { google } = require("googleapis");
+const fs = require("fs");
+const path = require("path");
+const mysqldump = require("mysqldump");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const admin = require("firebase-admin");
 const { getMessaging } = require("firebase-admin/messaging");
 const app = express();
 
-
 var serviceAccount = require("./pmc-cskh-firebase-adminsdk-y7378-5122f6edc7.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+});
+
+const credentials = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+};
+
+
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
+
+const auth = new google.auth.GoogleAuth({
+  credentials: credentials,
+  scopes: SCOPES,
+});
+const drive = google.drive({
+  version: "v3",
+  auth: auth,
 });
 
 var corsOptions = {
@@ -68,22 +97,116 @@ app.get("/test-noti", async (req, res) => {
     });
 });
 
-app.post('/generate-pdf', (req, res) => {
+app.post("/generate-pdf", (req, res) => {
   const doc = new PDFDocument();
 
   // Set the response type to PDF
-  res.setHeader('Content-Type', 'application/pdf');
-  
+  res.setHeader("Content-Type", "application/pdf");
+
   // Pipe the PDF into the response
   doc.pipe(res);
 
   // Add content to the PDF
-  doc.fontSize(25).text('Hello World!', 100, 100);
-  doc.text('This is a sample PDF generated on the server.');
-  
+  doc.fontSize(25).text("Hello World!", 100, 100);
+  doc.text("This is a sample PDF generated on the server.");
+
   // Finalize the PDF and end the stream
   doc.end();
 });
+
+async function exportDatabase() {
+  const backupPath = path.join(
+    __dirname,
+    `backup_checklist_${new Date().toISOString().slice(0, 10)}.sql`
+  );
+
+  await mysqldump({
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
+    },
+    dumpToFile: backupPath,
+  });
+
+  console.log(`Database exported to: ${backupPath}`);
+  return backupPath;
+}
+
+// Hàm upload file lên Google Drive
+async function uploadFile(filePath) {
+  try {
+    // ID của thư mục trên Google Drive
+    const folderId = "1TAMvnXHdhkTov68oKrLbB6DE0bVZezAL"; 
+
+    const createFile = await drive.files.create({
+      requestBody: {
+        name: path.basename(filePath), 
+        mimeType: "application/sql", 
+        parents: [folderId], 
+      },
+      media: {
+        mimeType: "application/sql",
+        body: fs.createReadStream(filePath),
+      },
+    });
+
+    const fileId = createFile.data.id;
+    console.log(`File uploaded with ID: ${fileId}`);
+
+    // Đặt quyền cho file
+    const getUrl = await setFilePublic(fileId);
+    console.log(getUrl.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Hàm đặt quyền công khai cho file
+async function setFilePublic(fileId) {
+  try {
+    await drive.permissions.create({
+      fileId,
+      requestBody: {
+        role: "reader",
+        type: "anyone",
+      },
+    });
+
+    const getUrl = await drive.files.get({
+      fileId,
+      fields: "webViewLink, webContentLink",
+    });
+
+    return getUrl;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Hàm thực hiện toàn bộ quá trình
+async function handleBackup() {
+  try {
+    const backupFilePath = await exportDatabase(); // Xuất cơ sở dữ liệu
+    await uploadFile(backupFilePath); // Upload file lên Google Drive
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+cron.schedule("0 4 * * *", async () => {
+  console.log("Running Cron Job at 4 AM");
+  try {
+    // Gọi hàm main của bạn
+    await handleBackup();
+    console.log("Cron job completed successfully");
+  } catch (error) {
+    console.error("Error running cron job:", error);
+  }
+});
+
+
 require("./app/routes/ent_calv.route")(app);
 require("./app/routes/ent_user.route")(app);
 require("./app/routes/ent_tang.route")(app);
