fd87f2ce91292dadf6ee7c23be877b355dcfbec1
bqm3 <bqminh30@gmail.com>
2024-10-23 14:46:11 +0700

    fix delte api

--- app/routes/ent_user.route.js
+++ app/routes/ent_user.route.js
@@ -10,13 +10,13 @@ module.exports = (app) => {
     router.post("/login", ent_user.login);
     router.get("/get-online",[isAuthenticated], ent_user.getUserOnline);
     router.get("/gs",[isAuthenticated], ent_user.getGiamSat);
-    router.get("/:id",[isAuthenticated, isAdmin], ent_user.getDetail);
+    router.get("/:id",[isAuthenticated], ent_user.getDetail);
     router.post("/check-auth",[isAuthenticated], ent_user.checkAuth);
     router.post("/device-token",[isAuthenticated], ent_user.deviceToken);
     router.post("/register", [isAuthenticated],ent_user.register);
     router.post("/change-password",[isAuthenticated], ent_user.changePassword);
-    router.put("/delete/:id",[isAuthenticated, isAdmin], ent_user.deleteUser);
-    router.put("/update/:id",[isAuthenticated, isAdmin], ent_user.updateUser);
+    router.put("/delete/:id",[isAuthenticated], ent_user.deleteUser);
+    router.put("/update/:id",[isAuthenticated], ent_user.updateUser);
     router.post("/noti",[isAuthenticated], ent_user.notiPush);
 
     router.post("/uploads", [isAuthenticated, upload.single('files')], ent_user.uploadFileUsers)
