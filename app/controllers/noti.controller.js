7d6fcb80e8385447c54d1f0a51d9d975ad14df31
manh2903 <manhdz22pl@gmail.com>
2024-11-01 18:29:28 +0700

    fix api noti

--- app/controllers/noti.controller.js
+++ app/controllers/noti.controller.js
@@ -6,18 +6,64 @@ const fetch = require("node-fetch");
 const moment = require("moment-timezone");
 
 exports.notiAll = async (req, res) => {
-    // 0 la k goi thong bao
-    // 1 la goi thong bao
-  const resData = {
-    key: '2.0.6',
-    type: "WARNING",
-    textTitle: "PMC Checklist",
-    textBody: "Phiên bản 2.0.6 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
-    time: 5000
-  };
-  return res.status(200).json({
-    message: "Thành công",
-    data: resData,
-  });
+  if(Object.keys(req.query).length === 0){
+    let loadingIOS = true 
+    let loadingAndroid = true 
+    let resData ;
+    if(loadingIOS == false){
+      resData = {
+        key: '2.0.6',
+        type: "WARNING",
+        textTitle: "PMC Checklist",
+        textBody: "Phiên bản 2.0.6 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
+        time: 5000
+      };
+    }
+    if(loadingAndroid == false){
+      resData = {
+        key: '2.0.6',
+        type: "WARNING",
+        textTitle: "PMC Checklist",
+        textBody: "Phiên bản 2.0.6 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
+        time: 5000
+      };
+    }
+    return res.status(200).json({
+      message: "Thành công",
+      data: resData,
+    });
+
+  } else {
+    const { version, platform } = req.query;
+    const keyVersionIOS = "2.0.6";
+    const keyVersionAPK = "2.0.6";
+
+    let ischeck = false; 
+    let resData = "";
+    let status = 0;
+  
+    if (
+      (platform === "ios" && version === keyVersionIOS) ||
+      (platform !== "ios" && version === keyVersionAPK)
+    ) {
+      ischeck = true;
+    }
+  
+    if(ischeck == false){
+      resData = {
+        type: "WARNING",
+        textTitle: "PMC Checklist",
+        textBody: "Phiên bản 2.0.6 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
+        time: 5000,
+      };
+      status = "1"
+    }
+  
+    return res.status(200).json({
+      message: "Thành công",
+      status: status,
+      data: resData,
+    });
+  }
 };
 //1461, 1498
