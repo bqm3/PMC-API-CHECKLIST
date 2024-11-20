1f141101ed1634245e82a7cc15377bce92cf477f
bqm3 <bqminh30@gmail.com>
2024-11-20 17:01:03 +0700

    update sơ eror exxcel

--- app/controllers/ent_all.controller.js
+++ app/controllers/ent_all.controller.js
@@ -244,12 +244,12 @@ exports.checkDateReportData = async (req, res) => {
 };
 
 function convertExcelDateTime(excelDate) {
-  if (typeof excelDate !== 'number' || isNaN(excelDate)) {
-    throw new Error('Invalid Excel date value');
+  if (typeof excelDate !== "number" || isNaN(excelDate)) {
+    throw new Error("Invalid Excel date value");
   }
 
   // Excel epoch starts from 1900-01-01
-  const excelEpoch = new Date(Date.UTC(1900, 0, 1)); 
+  const excelEpoch = new Date(Date.UTC(1900, 0, 1));
   const days = Math.floor(excelDate); // Phần nguyên: số ngày
   const fractionalDay = excelDate - days; // Phần thập phân: thời gian trong ngày
 
@@ -263,11 +263,11 @@ function convertExcelDateTime(excelDate) {
 
   // Định dạng ngày giờ đầy đủ
   const year = excelEpoch.getUTCFullYear();
-  const month = String(excelEpoch.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(excelEpoch.getUTCDate()).padStart(2, '0');
-  const hour = String(hours).padStart(2, '0');
-  const minute = String(minutes).padStart(2, '0');
-  const second = String(seconds).padStart(2, '0');
+  const month = String(excelEpoch.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(excelEpoch.getUTCDate()).padStart(2, "0");
+  const hour = String(hours).padStart(2, "0");
+  const minute = String(minutes).padStart(2, "0");
+  const second = String(seconds).padStart(2, "0");
 
   return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 }
@@ -302,160 +302,130 @@ exports.uploadFiles = async (req, res) => {
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
     const data = xlsx.utils.sheet_to_json(worksheet);
+
     await sequelize.transaction(async (transaction) => {
       const uppercaseKeys = (obj) => {
         return Object.keys(obj).reduce((acc, key) => {
-          const newKey = key.toUpperCase(); // Chuyển đổi key sang chữ in hoa
-          acc[newKey] = obj[key]; // Gán giá trị tương ứng
+          const newKey = key.toUpperCase();
+          acc[newKey] = obj[key];
           return acc;
         }, {});
       };
+
       for (const item of data) {
-        console.log('item["Ngày ghi nhận"]', item["Ngày ghi nhận"])
-        const tranforItem = uppercaseKeys(item);
-        const ngayGhiNhan = convertExcelDate(item["Ngày ghi nhận"]);
-        const tenDuAn = tranforItem["TÊN DỰ ÁN"];
+        try {
+          console.log('item["Ngày ghi nhận"]', item["Ngày ghi nhận"]);
+          const tranforItem = uppercaseKeys(item);
+          const ngayGhiNhan = convertExcelDate(item["Ngày ghi nhận"]);
+          const tenDuAn = tranforItem["TÊN DỰ ÁN"];
+          const nguoiTao = tranforItem["NGƯỜI TẠO"];
+          const created = convertExcelDateTime(item["Created"]);
+          const dienCuDan = tranforItem["SỐ ĐIỆN TIÊU THỤ HÔM QUA (CƯ DÂN)"];
+          const dienCdt = tranforItem["SỐ ĐIỆN TIÊU THỤ HÔM QUA (CĐT)"];
+          const nuocCuDan = tranforItem["SỐ NƯỚC TIÊU THỤ HÔM QUA (CƯ DÂN)"];
+          const nuocCdt = tranforItem["SỐ NƯỚC TIÊU THỤ HÔM QUA (CĐT)"];
+          const nuocXaThai = tranforItem["NƯỚC XẢ THẢI HÔM QUA"];
+          const racSinhHoat = tranforItem["RÁC SINH HOẠT HÔM QUA"];
+          const tuiRac240 = tranforItem["TÚI ĐỰNG RÁC 240 LIT HÔM QUA"];
+          const tuiRac120 = tranforItem["TÚI ĐỰNG RÁC 120 LIT"];
+          const tuiRac20 = tranforItem["TÚI ĐỰNG RÁC 20 LIT"];
+          const tuiRac10 = tranforItem["TÚI ĐỰNG RÁC 10 LIT"];
+          const tuiRac5 = tranforItem["TÚI ĐỰNG RÁC 5 LIT"];
+          const giayVs235 = tranforItem["GIẤY VỆ SINH 90X235MM"];
+          const giayVs120 = tranforItem["GIẤY VỆ SINH 90X120MM"];
+          const giayLauTay = tranforItem["GIẤY LAU TAY"];
+          const hoaChat = tranforItem["HÓA CHẤT LÀM SẠCH"];
+          const nuocRuaTay = tranforItem["NƯỚC RỬA TAY"];
+          const nhietDo = tranforItem["NHIỆT ĐỘ MÔI TRƯỜNG"];
+          const nuocBu = tranforItem["NƯỚC BÙ BỂ BƠI"];
+          const clo = tranforItem["NỒNG ĐỘ CLO BỂ BƠI"];
+          const ph = tranforItem["NỒNG ĐỘ PH BỂ BƠI"];
+          const dauMay = tranforItem["DẦU MÁY PHÁT"];
+          const tratThai = tranforItem["TRẠT THẢI XÂY DỰNG"];
+          const chiSoCo2 = tranforItem["CHỈ SỐ CO2 TẠI TẦNG HẦM"];
+          const chlorine90 = tranforItem["CHLORINE 90%"];
+          const axitHcl = tranforItem["A XÍT HCL"];
+          const chlorine90Vien = tranforItem["CHLORINE 90% (DẠNG VIÊN)"];
+          const phMinus = tranforItem["PH MINUS"];
+          const poolBlock = tranforItem["POOL BLOCK"];
+          const pn180 = tranforItem["PN180"];
+          const muoiDienPhan = tranforItem["MUỐI ĐIỆN PHÂN"];
+          const pac = tranforItem["PAC"];
+          const naHSO3 = tranforItem["NAHSO3"];
+          const naOH = tranforItem["NAOH"];
+          const matRiDuong = tranforItem["MẬT RỈ ĐƯỜNG"];
+          const polymerAnion = tranforItem["POLYMER ANION"];
+          const methanol = tranforItem["METHANOL"];
+          const modified = convertExcelDateTime(item["Modified"]);
+          const modifiedBy = tranforItem["MODIFIED BY"];
+          const email = tranforItem["EMAIL"];
 
-        const nguoiTao = tranforItem["NGƯỜI TẠO"];
+          console.log("tenDuAn", tenDuAn);
 
-        const created =  convertExcelDateTime(item["Created"]);
-
-        const dienCuDan = tranforItem["SỐ ĐIỆN TIÊU THỤ HÔM QUA (CƯ DÂN)"];
-
-        const dienCdt = tranforItem["SỐ ĐIỆN TIÊU THỤ HÔM QUA (CĐT)"];
-
-        const nuocCuDan = tranforItem["SỐ NƯỚC TIÊU THỤ HÔM QUA (CƯ DÂN)"];
-
-        const nuocCdt = tranforItem["SỐ NƯỚC TIÊU THỤ HÔM QUA (CĐT)"];
-
-        const nuocXaThai = tranforItem["NƯỚC XẢ THẢI HÔM QUA"];
-
-        const racSinhHoat = tranforItem["RÁC SINH HOẠT HÔM QUA"];
-
-        const tuiRac240 = tranforItem["TÚI ĐỰNG RÁC 240 LIT HÔM QUA"];
-
-        const tuiRac120 = tranforItem["TÚI ĐỰNG RÁC 120 LIT"];
-
-        const tuiRac20 = tranforItem["TÚI ĐỰNG RÁC 20 LIT"];
-
-        const tuiRac10 = tranforItem["TÚI ĐỰNG RÁC 10 LIT"];
-
-        const tuiRac5 = tranforItem["TÚI ĐỰNG RÁC 5 LIT"];
-
-        const giayVs235 = tranforItem["GIẤY VỆ SINH 90X235MM"];
-
-        const giayVs120 = tranforItem["GIẤY VỆ SINH 90X120MM"];
-
-        const giayLauTay = tranforItem["GIẤY LAU TAY"];
-
-        const hoaChat = tranforItem["HÓA CHẤT LÀM SẠCH"];
-
-        const nuocRuaTay = tranforItem["NƯỚC RỬA TAY"];
-
-        const nhietDo = tranforItem["NHIỆT ĐỘ MÔI TRƯỜNG"];
-
-        const nuocBu = tranforItem["NƯỚC BÙ BỂ BƠI"];
-        const clo = tranforItem["NỒNG ĐỘ CLO BỂ BƠI"];
-
-        const ph = tranforItem["NỒNG ĐỘ PH BỂ BƠI"];
-
-        const dauMay = tranforItem["DẦU MÁY PHÁT"];
-
-        const tratThai = tranforItem["TRẠT THẢI XÂY DỰNG"];
-
-        const chiSoCo2 = tranforItem["CHỈ SỐ CO2 TẠI TẦNG HẦM"];
-
-        const chlorine90 = tranforItem["CHLORINE 90%"];
-
-        const axitHcl = tranforItem["A XÍT HCL"];
-        const chlorine90Vien = tranforItem["CHLORINE 90% (DẠNG VIÊN)"];
-
-        const phMinus = tranforItem["PH MINUS"];
-
-        const poolBlock = tranforItem["POOL BLOCK"];
-
-        const pn180 = tranforItem["PN180"];
-
-        const muoiDienPhan = tranforItem["MUỐI ĐIỆN PHÂN"];
-
-        const pac = tranforItem["PAC"];
-
-        const naHSO3 = tranforItem["NAHSO3"];
-
-        const naOH = tranforItem["NAOH"];
-
-        const matRiDuong = tranforItem["MẬT RỈ ĐƯỜNG"];
-
-        const polymerAnion = tranforItem["POLYMER ANION"];
-
-        const methanol = tranforItem["METHANOL"];
-
-        const modified = convertExcelDateTime(item["Modified"]);
-
-        const modifiedBy = tranforItem["MODIFIED BY"];
-
-        const email = tranforItem["EMAIL"];
-
-        console.log('tenDuAn', tenDuAn)
-        const newHSSE = await hsse.create(
-          {
-            Ten_du_an: tenDuAn,
-            Ngay_ghi_nhan: ngayGhiNhan,
-            Nguoi_tao: nguoiTao,
-            Dien_cu_dan: dienCuDan,
-            Dien_cdt: dienCdt,
-            Nuoc_cu_dan: nuocCuDan,
-            Nuoc_cdt: nuocCdt,
-            Xa_thai: nuocXaThai,
-            Rac_sh: racSinhHoat,
-            Muoi_dp: muoiDienPhan,
-            Pac: pac,
-            NaHSO3: naHSO3,
-            NaOH: naOH,
-            Mat_rd: matRiDuong,
-            Polymer_Anion: polymerAnion,
-            Chlorine_bot: chlorine90,
-            Chlorine_vien: chlorine90Vien,
-            Methanol: methanol,
-            Dau_may: dauMay,
-            Tui_rac240: tuiRac240,
-            Tui_rac120: tuiRac120,
-            Tui_rac20: tuiRac20,
-            Tui_rac10: tuiRac10,
-            Tui_rac5: tuiRac5,
-            giayvs_235: giayVs235,
-            giaivs_120: giayVs120,
-            giay_lau_tay: giayLauTay,
-            hoa_chat: hoaChat,
-            nuoc_rua_tay: nuocRuaTay,
-            nhiet_do: nhietDo,
-            nuoc_bu: nuocBu,
-            clo: clo,
-            PH: ph,
-            Poolblock: poolBlock,
-            trat_thai: tratThai,
-            Email: email,
-            pHMINUS: phMinus,
-            axit: axitHcl,
-            PN180: pn180,
-            modifiedBy: modifiedBy,
-            chiSoCO2: chiSoCo2,
-            updatedAt: modified,
-            createdAt: created,
-            
-            
-          },
-          { transaction }
-        );
+          const newHSSE = await hsse.create(
+            {
+              Ten_du_an: tenDuAn,
+              Ngay_ghi_nhan: ngayGhiNhan,
+              Nguoi_tao: nguoiTao,
+              Dien_cu_dan: dienCuDan,
+              Dien_cdt: dienCdt,
+              Nuoc_cu_dan: nuocCuDan,
+              Nuoc_cdt: nuocCdt,
+              Xa_thai: nuocXaThai,
+              Rac_sh: racSinhHoat,
+              Muoi_dp: muoiDienPhan,
+              Pac: pac,
+              NaHSO3: naHSO3,
+              NaOH: naOH,
+              Mat_rd: matRiDuong,
+              Polymer_Anion: polymerAnion,
+              Chlorine_bot: chlorine90,
+              Chlorine_vien: chlorine90Vien,
+              Methanol: methanol,
+              Dau_may: dauMay,
+              Tui_rac240: tuiRac240,
+              Tui_rac120: tuiRac120,
+              Tui_rac20: tuiRac20,
+              Tui_rac10: tuiRac10,
+              Tui_rac5: tuiRac5,
+              giayvs_235: giayVs235,
+              giaivs_120: giayVs120,
+              giay_lau_tay: giayLauTay,
+              hoa_chat: hoaChat,
+              nuoc_rua_tay: nuocRuaTay,
+              nhiet_do: nhietDo,
+              nuoc_bu: nuocBu,
+              clo: clo,
+              PH: ph,
+              Poolblock: poolBlock,
+              trat_thai: tratThai,
+              Email: email,
+              pHMINUS: phMinus,
+              axit: axitHcl,
+              PN180: pn180,
+              modifiedBy: modifiedBy,
+              chiSoCO2: chiSoCo2,
+              updatedAt: modified,
+              createdAt: created,
+            },
+            { transaction }
+          );
+        } catch (err) {
+          console.error("Error processing item:", item, "Error:", err);
+          throw err; // Rethrow error to exit transaction on failure
+        }
       }
     });
+
     res.send({
       message: "File uploaded and data processed successfully",
       data,
     });
   } catch (err) {
+    console.error("Error in file upload:", err);
     return res.status(500).json({
       message: err.message || "Lỗi! Vui lòng thử lại sau.",
     });
   }
 };
+
