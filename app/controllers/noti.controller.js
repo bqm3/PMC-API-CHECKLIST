336d9b7bcbdfdb8f74368989f82f2380cb59f5db
bqm3 <bqminh30@gmail.com>
2024-12-12 17:34:21 +0700

    add version 1.1

--- app/controllers/noti.controller.js
+++ app/controllers/noti.controller.js
@@ -1,62 +1,33 @@
 exports.notiAll = async (req, res) => {
-  if(Object.keys(req.query).length === 0){
-    let loadingIOS = false 
-    let loadingAndroid = false 
-    let resData ;
-    if(loadingIOS == false){
-      resData = {
-        key: '2.0.7',
-        type: "WARNING",
-        textTitle: "PMC Checklist",
-        textBody: "Phiên bản 2.0.7 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
-        time: 5000
-      };
-    }
-    if(loadingAndroid == false){
-      resData = {
-        key: '2.0.8',
-        type: "WARNING",
-        textTitle: "PMC Checklist",
-        textBody: "Phiên bản 2.0.8 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
-        time: 5000
-      };
-    }
-    return res.status(200).json({
-      message: "Thành công",
-      data: resData,
-    });
+  const { version, platform } = req.query;
+  const keyVersionIOS = "2.1.1";
+  const keyVersionAPK = "2.1.1";
 
-  } else {
-    const { version, platform } = req.query;
-    const keyVersionIOS = "2.1.0";
-    const keyVersionAPK = "2.1.0";
+  let ischeck = false;
+  let resData = "";
+  let status = 0;
 
-    let ischeck = false; 
-    let resData = "";
-    let status = 0;
-  
-    if (
-      (platform === "ios" && version === keyVersionIOS) ||
-      (platform !== "ios" && version === keyVersionAPK)
-    ) {
-      ischeck = true;
-    }
-  
-    if(ischeck == false){
-      resData = {
-        type: "WARNING",
-        textTitle: "PMC Checklist",
-        textBody: "Phiên bản 2.1.0 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
-        time: 5000,
-      };
-      status = "1"
-    }
-  
-    return res.status(200).json({
-      message: "Thành công",
-      status: status,
-      data: resData,
-    });
+  if (
+    (platform === "ios" && version === keyVersionIOS) ||
+    (platform !== "ios" && version === keyVersionAPK)
+  ) {
+    ischeck = true;
   }
+
+  if (ischeck == false) {
+    resData = {
+      type: "WARNING",
+      textTitle: "PMC Checklist",
+      textBody:
+        "Phiên bản 2.1.0 đã xuất bản. Cập nhật phiên bản để có trải nghiệm tốt nhất.",
+      time: 5000,
+    };
+    status = "1";
+  }
+
+  return res.status(200).json({
+    message: "Thành công",
+    status: status,
+    data: resData,
+  });
 };
-//1461, 1498
