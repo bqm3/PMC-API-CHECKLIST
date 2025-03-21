daee98cee5d2ffe39c513e2f82922321d989c280
bqm3 <bqminh30@gmail.com>
2025-03-21 16:08:20 +0700

    fix name folder

--- app/routes/ent_thamsophanhe.routes.js
+++ app/routes/ent_thamsophanhe.routes.js
@@ -1,20 +1,17 @@
 module.exports = (app) => {
-    const multer = require("multer");
-    const upload = multer();
-    const ent_thamsophanhe = require("../controllers/hsse/ent_thamsophanhe.controller.js");
-    const {
-      isAuthenticated,
-      isAdmin,
-    } = require("../middleware/auth_middleware.js");
-    const logAction = require("../middleware/log_action.js");
-    
-  
-    var router = require("express").Router();
-  
-    router.post("/uploads",upload.single('files'), ent_thamsophanhe.uploads);
-    router.get("/getDetail", isAuthenticated, ent_thamsophanhe.getDetail)
+  const multer = require("multer");
+  const upload = multer();
+  const ent_thamsophanhe = require("../controllers/Hsse/ent_thamsophanhe.controller.js");
+  const {
+    isAuthenticated,
+    isAdmin,
+  } = require("../middleware/auth_middleware.js");
+  const logAction = require("../middleware/log_action.js");
 
-  
-    app.use("/api/v2/ent_thamsophanhe", router);
-  };
-  
+  var router = require("express").Router();
+
+  router.post("/uploads", upload.single("files"), ent_thamsophanhe.uploads);
+  router.get("/getDetail", isAuthenticated, ent_thamsophanhe.getDetail);
+
+  app.use("/api/v2/ent_thamsophanhe", router);
+};
